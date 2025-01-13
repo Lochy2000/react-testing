90535b225add2e6b32a82eaf3ec768638ae7569d
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-01-13 12:42:37 +0000

    functional stateless components

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetings from "./components/FunctionsGreetings"
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetings/>
+    </div>
+  );
+}
+
+export default App;
